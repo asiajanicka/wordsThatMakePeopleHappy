8f337238dfa2b41c00309171ff025a338d272ca4
asiajanicka <asia.janicka@gmil.com>
2022-12-22 12:38:59 +0100

    added common method for repeated code in both countWords method

--- src/main/java/org/jjm/Utils.java
+++ src/main/java/org/jjm/Utils.java
@@ -1,14 +1,12 @@
 package org.jjm;
 
-import org.apache.commons.lang3.StringUtils;
-
-import javax.swing.text.html.parser.Entity;
 import java.util.*;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Utils {
+
     /**
      * Method treats words with hyphen (like ex-wife) and apostrophe (like it's) as single
      *
@@ -16,7 +14,22 @@ public class Utils {
      * @return map with word & number of occurrences as key-value pairs
      */
     public static HashMap<String, Long> countWordsWithHyphenAndApostrophe(String content) {
-        List<String> words = Pattern.compile("\\w+[-'\\w*]*")
+        return countWordsByPattern("\\w+[-'\\w*]*", content);
+    }
+
+
+    /**
+     * Method treats words with hyphen (like ex-wife) and apostrophe (like it's) as separate words
+     *
+     * @param content is a text where we count words in
+     * @return map with word & number of occurrences as key-value pairs
+     */
+    public static HashMap<String, Long> countWords(String content) {
+        return countWordsByPattern("\\w+", content);
+    }
+
+    private static HashMap<String, Long> countWordsByPattern(String pattern, String content) {
+        List<String> words = Pattern.compile(pattern)
                 .matcher(content)
                 .results()
                 .map(MatchResult::group)
@@ -29,24 +42,9 @@ public class Utils {
         return countMap;
     }
 
-    /**
-     * Method treats words with hyphen (like ex-wife) and apostrophe (like it's) as separate words
-     *
-     * @param content is a text where we count words in
-     * @return map with word & number of occurrences as key-value pairs
-     */
-    public static HashMap<String, Long> countWords(String content) {
-        String cont = content.replaceAll("\\W", " ").replaceAll("\\s+", ",");
-        List<String> words = Arrays.asList(StringUtils.split(cont, ","));
-        HashMap<String, Long> countMap = new HashMap<>();
-        for (String word : words) {
-            countMap.put(word, words.stream().filter(w -> w.equals(word)).count());
-        }
-        return countMap;
-    }
-
-    public static List<Map.Entry<String,Long>> sortByValue(Map<String, Long> map) {
+    public static List<Map.Entry<String, Long>> sortByValue(Map<String, Long> map) {
         return map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .collect(Collectors.toList());
     }
+
 }
